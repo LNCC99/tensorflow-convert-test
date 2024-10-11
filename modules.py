ab85a938c8a89f25e2f5ec3e29f8c9ecc4fe0a30
LNCC99 <nana_chenggong@sina.com>
2024-10-11 17:38:30 +0800

    Add files via upload
    update convert to tflite test code

--- modules.py
+++ modules.py
@@ -8,6 +8,7 @@ Created on Tue Jan 12 14:53:48 2021
 import tensorflow.keras as keras
 import tensorflow as tf
 
+layernorm_enable = False
 '''
 dual path rnn block
 '''   
@@ -30,20 +31,20 @@ class DprnnBlock(keras.layers.Layer):
         self.intra_rnn = keras.layers.Bidirectional(keras.layers.LSTM(units=self.numUnits//2, return_sequences=True,implementation = 2,recurrent_activation = 'sigmoid'))
        
         self.intra_fc = keras.layers.Dense(units = self.numUnits,)
-
-        if self.causal:
-            self.intra_ln = keras.layers.LayerNormalization(center=True, scale=True, axis = [-1,-2])
-        else:
-            self.intra_ln = keras.layers.LayerNormalization(center=False, scale=False)
+        if layernorm_enable:
+            if self.causal:
+                self.intra_ln = keras.layers.LayerNormalization(center=True, scale=True, axis = [-1,-2])
+            else:
+                self.intra_ln = keras.layers.LayerNormalization(center=False, scale=False)
 
         #self.inter_rnn = keras.layers.LSTM(units=self.numUnits, return_sequences=True,implementation = 2,recurrent_activation = 'hard_sigmoid')
         self.inter_rnn = keras.layers.LSTM(units=self.numUnits, return_sequences=True,implementation = 2,recurrent_activation = 'sigmoid')
         self.inter_fc = keras.layers.Dense(units = self.numUnits,) 
-
-        if self.causal:
-            self.inter_ln = keras.layers.LayerNormalization(center=True, scale=True, axis = [-1,-2])
-        else:
-            self.inter_ln = keras.layers.LayerNormalization(center=False, scale=False)
+        if layernorm_enable:
+            if self.causal:
+                self.inter_ln = keras.layers.LayerNormalization(center=True, scale=True, axis = [-1,-2])
+            else:
+                self.inter_ln = keras.layers.LayerNormalization(center=False, scale=False)
 
         self.L = L
         self.width = width
@@ -58,10 +59,12 @@ class DprnnBlock(keras.layers.Layer):
         
         intra_rnn = self.intra_rnn
         intra_fc = self.intra_fc
-        intra_ln = self.intra_ln
+        if layernorm_enable:
+            intra_ln = self.intra_ln
         inter_rnn = self.inter_rnn
         inter_fc = self.inter_fc
-        inter_ln = self.inter_ln
+        if layernorm_enable:
+            inter_ln = self.inter_ln
         channel = self.channel
         causal = self.causal
         
@@ -73,17 +76,21 @@ class DprnnBlock(keras.layers.Layer):
         
         # (bs*T,F,C) channel axis dense
         intra_dense_out = intra_fc(intra_LSTM_out)
-            
-        if causal:
-            # (bs*T,F,C) --> (bs,T,F,C) Freq and channel norm
+        
+        if layernorm_enable:   
+            if causal:
+                # (bs*T,F,C) --> (bs,T,F,C) Freq and channel norm
+                intra_ln_input = tf.reshape(intra_dense_out,[batch_size,-1,width,channel])
+                intra_out = intra_ln(intra_ln_input)
+                
+            else:       
+                # (bs*T,F,C) --> (bs,T*F*C) global norm
+                intra_ln_input = tf.reshape(intra_dense_out,[batch_size,-1])
+                intra_ln_out = intra_ln(intra_ln_input)
+                intra_out = tf.reshape(intra_ln_out,[batch_size,L,width,channel])
+        else:
             intra_ln_input = tf.reshape(intra_dense_out,[batch_size,-1,width,channel])
-            intra_out = intra_ln(intra_ln_input)
-            
-        else:       
-            # (bs*T,F,C) --> (bs,T*F*C) global norm
-            intra_ln_input = tf.reshape(intra_dense_out,[batch_size,-1])
-            intra_ln_out = intra_ln(intra_ln_input)
-            intra_out = tf.reshape(intra_ln_out,[batch_size,L,width,channel])
+            intra_out = intra_ln_input
         # (bs,T,F,C)
         intra_out = keras.layers.Add()([x,intra_out])
         #%% Inter-Chunk Processing
@@ -98,19 +105,22 @@ class DprnnBlock(keras.layers.Layer):
         inter_dense_out = inter_fc(inter_LSTM_out)
         
         inter_dense_out = tf.reshape(inter_dense_out,[batch_size,width,L,channel])
-        
-        if causal:
-            # (bs,F,T,C) --> (bs,T,F,C)
-            inter_ln_input = tf.transpose(inter_dense_out,[0,2,1,3])
-            inter_out = inter_ln(inter_ln_input)
-            
+        if layernorm_enable:
+            if causal:
+                # (bs,F,T,C) --> (bs,T,F,C)
+                inter_ln_input = tf.transpose(inter_dense_out,[0,2,1,3])
+                inter_out = inter_ln(inter_ln_input)
+                
+            else:
+                # (bs,F,T,C) --> (bs,F*T*C)
+                inter_ln_input = tf.reshape(inter_dense_out,[batch_size,-1])
+                inter_ln_out = inter_ln(inter_ln_input)
+                inter_out = tf.reshape(inter_ln_out,[batch_size,width,L,channel])
+                # (bs,F,T,C) --> (bs,T,F,C)
+                inter_out = tf.transpose(inter_out,[0,2,1,3])
         else:
-            # (bs,F,T,C) --> (bs,F*T*C)
-            inter_ln_input = tf.reshape(inter_dense_out,[batch_size,-1])
-            inter_ln_out = inter_ln(inter_ln_input)
-            inter_out = tf.reshape(inter_ln_out,[batch_size,width,L,channel])
-            # (bs,F,T,C) --> (bs,T,F,C)
-            inter_out = tf.transpose(inter_out,[0,2,1,3])
+            inter_ln_input = tf.transpose(inter_dense_out,[0,2,1,3])
+            inter_out = inter_ln_input
         # (bs,T,F,C)
         inter_out = keras.layers.Add()([intra_out,inter_out])
     
---
+++ tensorflow_convert2tflite.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+import tflite_runtime.interpreter as tflite
+from dpcrn_model import DPCRN_model
+import os
+import numpy as np
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0' 
+def representative_data_gen():
+   for _ in range(2):
+      yield [np.random.rand(1,3,201,2).astype(np.float32)]
+      
+
+dpcrn = DPCRN_model(batch_size = 1, 
+                    length_in_s = 5, 
+                    lr = 1e-3,
+                    block_len = 400,
+                    block_shift = 200)
+my_model = dpcrn.build_DPCRN_model()
+converter = tf.lite.TFLiteConverter.from_keras_model(my_model)
+      
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+#converter.target_spec.supported_ops = [tf.lite.OpsSet.EXPERIMENTAL_TFLITE_BUILTINS_ACTIVATIONS_INT16_WEIGHTS_INT8] #
+#converter.inference_input_type = tf.int16
+#converter.inference_output_type = tf.int16
+tflite_model = converter.convert()
+
+quant_model_path = './model_tflite_CTIVATIONS_INT16_WEIGHTS_INT8_InOutInt16.tflite'
+with open(quant_model_path, 'wb') as f:
+    f.write(tflite_model)
+#interpreter = tf.lite.Interpreter(model_path = tflite_model)
+interpreter = tflite.Interpreter(model_path=quant_model_path)
+interpreter.allocate_tensors() #加载所有的tensor
+
+input_type = interpreter.get_input_details()[0]['dtype']
+print(interpreter.get_input_details())
+print('input type:',input_type)
+output_type = interpreter.get_output_details()[0]['dtype']
+print(interpreter.get_output_details())
+print('output type:',output_type)
+print('/n')
+tensor_details = interpreter.get_tensor_details()
+for tensor in tensor_details:
+    #print(tensor)
+    print(f"Tensor Name:{tensor['name']}")
+    print(f"Tensor Shape:{tensor['shape']}")
+    print(f"Tensor Type：{tensor['dtype']}")
